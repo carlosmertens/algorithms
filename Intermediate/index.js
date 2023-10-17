9b5afa2f9457c800872e0ca82e62e60552fbaaa2
Carlos Mertens <mertenscarlos3000@gmail.com>
2023-10-17 22:06:54 +0200

    Add WeakMap algorithm

--- Intermediate/index.js
+++ Intermediate/index.js
@@ -403,3 +403,24 @@ class HTMLClasses {
 
 // const div = new HTMLClasses('div', '', []);
 // console.log(div.showHTML()); // <div id="" class="">...</div>
+
+console.log('\n********** WEAKMAP HIDDEN PROPERTIES ***********');
+// Use WeakMap to hide a property
+// Set setters and getters
+
+const _radius = new WeakMap();
+
+class Oval {
+  constructor(radius) {
+    _radius.set(this, radius);
+  }
+
+  get radius() {
+    return _radius.get(this);
+  }
+
+  set radius(value) {
+    if (value <= 0) throw new Error('Invalid radius!');
+    _radius.set(this, value);
+  }
+}
