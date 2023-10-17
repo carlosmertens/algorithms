8e735eadde88302403142565eb4b20db19844512
Carlos Mertens <mertenscarlos3000@gmail.com>
2023-10-17 22:25:47 +0200

    Stack algorithm

--- Intermediate/index.js
+++ Intermediate/index.js
@@ -424,3 +424,36 @@ class Oval {
     _radius.set(this, value);
   }
 }
+
+console.log('\n********** STACK ALGORITHM ***********');
+// Use private properties
+
+const _stack = new WeakMap();
+
+class Stack {
+  constructor() {
+    _stack.set(this, []);
+  }
+
+  push(value) {
+    _stack.get(this).push(value);
+  }
+
+  pop() {
+    const items = _stack.get(this);
+    if (items.length === 0) throw new Error('Stack is empty!');
+
+    return items.pop();
+  }
+
+  peak() {
+    const items = _stack.get(this);
+    if (items.length === 0) throw new Error('Stack is empty!');
+
+    return items[items.length - 1];
+  }
+
+  get count() {
+    return _stack.get(this).length;
+  }
+}
