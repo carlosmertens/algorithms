45f16867ef5dfba3744c2e117f9088d6a47e6890
Carlos Mertens <mertenscarlos3000@gmail.com>
2023-10-18 15:24:14 +0200

    Create grade calculator file

---
+++ Basic/grade_calculator.js
@@ -0,0 +1,27 @@
+console.log('\n********** 3. Calculate Grade **********');
+// ***** 3. Function that calculates the grade of a studend in letters
+// 1-59: F
+// 60-69: D
+// 70-79: C
+// 80-89: B
+// 90-100: A
+
+const marks = [90, 80, 100];
+// console.log(calculateGrade(marks));
+
+function calculateGrade(arr) {
+  const avg = calculateAverage(arr);
+
+  if (avg < 60) grade = 'F';
+  else if (avg < 70) return 'D';
+  else if (avg < 80) return 'C';
+  else if (avg < 90) return 'B';
+  else return 'A';
+}
+
+function calculateAverage(arr) {
+  let sum = 0;
+  for (const value of arr) sum += value;
+
+  return sum / arr.length;
+}
--- Basic/index.js
+++ Basic/index.js
@@ -1,31 +1,3 @@
-console.log('\n********** 3. Calculate Grade **********');
-// ***** 3. Function that calculates the grade of a studend in letters
-// 1-59: F
-// 60-69: D
-// 70-79: C
-// 80-89: B
-// 90-100: A
-
-const marks = [90, 80, 100];
-// console.log(calculateGrade(marks));
-
-function calculateGrade(arr) {
-  const avg = calculateAverage(arr);
-
-  if (avg < 60) grade = 'F';
-  else if (avg < 70) return 'D';
-  else if (avg < 80) return 'C';
-  else if (avg < 90) return 'B';
-  else return 'A';
-}
-
-function calculateAverage(arr) {
-  let sum = 0;
-  for (const value of arr) sum += value;
-
-  return sum / arr.length;
-}
-
 console.log('\n********** 4. Show Primes **********');
 // ***** 4. showPrimes function returns prime numbers base on limit provided.
 
