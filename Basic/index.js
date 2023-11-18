c786d59f6e0689c4f3a92107c1556b01bf33f0d4
Carlos Mertens <mertenscarlos3000@gmail.com>
2023-11-18 13:33:33 +0100

    Add stack algorithm

--- Basic/index.js
+++ Basic/index.js
@@ -1,19 +1,5 @@
 'use strict';
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
 
-function findNumInArr(n, arr) {
-  let hasNum = false;
-
-  arr.forEach((el) => {
-    if (el === n) {
-      hasNum = true;
-    }
-
-    if (Array.isArray(el)) {
-      hasNum = findNumInArr(n, el);
-    }
-  });
-
-  return hasNum;
-}
-
-console.log(findNumInArr(7, [1, 3, 4, 6, [1, 4, [7]]]));
+console.log(airline.slice(airline.indexOf('Air')));
--- Intermediate/index.js
+++ Intermediate/index.js
@@ -196,39 +196,6 @@ class Oval {
   }
 }
 
-console.log('\n********** STACK ALGORITHM ***********');
-// Use private properties
-
-const _stack = new WeakMap();
-
-class Stack {
-  constructor() {
-    _stack.set(this, []);
-  }
-
-  push(value) {
-    _stack.get(this).push(value);
-  }
-
-  pop() {
-    const items = _stack.get(this);
-    if (items.length === 0) throw new Error('Stack is empty!');
-
-    return items.pop();
-  }
-
-  peak() {
-    const items = _stack.get(this);
-    if (items.length === 0) throw new Error('Stack is empty!');
-
-    return items[items.length - 1];
-  }
-
-  get count() {
-    return _stack.get(this).length;
-  }
-}
-
 console.log('\n********** Unique Array ***********');
 // Create own method to retur unique elements of an array
 // Add the method to the Array.prototype
